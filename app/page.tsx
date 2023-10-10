11d43132425a8ebbc2d1a9d586d4d284b22b373e
AayushMohan <aayushmohan1702@gmail.com>
2023-10-10 23:53:57 +0530

    Added Hero Msg

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="bg-[#F0f0F0] h-screen text-[#000]">
       <Header />
+      <Hero />
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,14 @@ import React from "react";
 type Props = {};
 
 const Hero = (props: Props) => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <h1>
+        {" "}
+        <span>The best</span> selection of furniture
+      </h1>
+    </div>
+  );
 };
 
 export default Hero;
