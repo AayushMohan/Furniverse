5cd3e48cd412e74da4eebb73a8386a25e3e0a8b4
AayushMohan <aayushmohan1702@gmail.com>
2023-10-10 21:42:25 +0530

    Added Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Furniverse</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Furniture</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
