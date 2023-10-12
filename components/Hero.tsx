0e7374ac40775b08ba344f9cb054ce0a0ddf3bb3
AayushMohan <aayushmohan1702@gmail.com>
2023-10-12 23:46:36 +0530

    Added Styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,42 +14,40 @@ const Hero = (props: Props) => {
         We make your life more comfortable with quality and comfortable
         furniture.
       </p>
-
-      <div>
-        <p>What do we provide to you?</p>
-      </div>
-
-      <div>
-        <h1>Rich Experience</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
-          adipisci?
-        </p>
-        <button>More Info</button>
-      </div>
-
-      <div>
-        <h1>Affordable Price</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-          nihil?
-        </p>
-        <button>More</button>
-      </div>
-
-      <div>
-        <h1>Many Choices</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, labore?
-        </p>
-        <button>More Info</button>
-      </div>
-
-      <div>
-        <h1>Shop By Category</h1>
-        <button>Living Room Chair</button>
-        <button>Chair</button>
-        <button>Arm Chair</button>
+      <div className="flex">
+        <div>
+          <p>What do we provide to you?</p>
+        </div>
+        <div>
+          <h1>Rich Experience</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+            adipisci?
+          </p>
+          <button>More Info</button>
+        </div>
+        <div>
+          <h1>Affordable Price</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+            nihil?
+          </p>
+          <button>More</button>
+        </div>
+        <div>
+          <h1>Many Choices</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            labore?
+          </p>
+          <button>More Info</button>
+        </div>
+        <div>
+          <h1>Shop By Category</h1>
+          <button>Living Room Chair</button>
+          <button>Chair</button>
+          <button>Arm Chair</button>
+        </div>
       </div>
     </div>
   );
