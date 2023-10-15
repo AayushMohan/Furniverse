9136b0af42c6fd67e330d512fed17f8d176f41eb
AayushMohan <aayushmohan1702@gmail.com>
2023-10-15 22:59:17 +0530

    Updated Styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,7 +38,7 @@ const Hero = (props: Props) => {
 
         <div>
           <h1 className="font-bold">Rich experience</h1>
-          <p className="text-[#383838]/50">
+          <p className="text-[#383838]/50 w-2/3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
             adipisci?
           </p>
