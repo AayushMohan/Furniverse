b446a3f38cd263a17a80d0f441ba640722dedd9b
AayushMohan <aayushmohan1702@gmail.com>
2023-10-15 23:03:53 +0530

    Updated Text

--- components/Category.tsx
+++ components/Category.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Category = (props: Props) => {
   return (
     <div className="h-screen bg-[#F0f0F0]">
-      <h1 className="text-3xl">Shop By Category</h1>
+      <h1 className="text-3xl text-center">Shop By Category</h1>
       <div>
         <button>Living Room Chair</button>
         <button>Chair</button>
