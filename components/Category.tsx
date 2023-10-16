7a4fba86828b4bc3aaf87c10549392a82cfe1c4c
AayushMohan <aayushmohan1702@gmail.com>
2023-10-16 23:08:18 +0530

    Added Styling

--- components/Category.tsx
+++ components/Category.tsx
@@ -6,7 +6,7 @@ const Category = (props: Props) => {
   return (
     <div className="h-screen bg-[#F0f0F0] pt-20">
       <h1 className="text-3xl text-center">Shop By Category</h1>
-      <div>
+      <div className="flex items-center justify-center">
         <button className="mx-4 p-2 bg-[#A5988E]/70 text-[#383838] rounded-2xl">
           Living Room
         </button>
