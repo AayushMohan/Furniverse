740085902816f495dce2527fd33821db405a3acb
AayushMohan <aayushmohan1702@gmail.com>
2023-10-16 23:01:53 +0530

    Styled Category

--- components/Category.tsx
+++ components/Category.tsx
@@ -7,7 +7,9 @@ const Category = (props: Props) => {
     <div className="h-screen bg-[#F0f0F0] pt-20">
       <h1 className="text-3xl text-center">Shop By Category</h1>
       <div>
-        <button>Living Room Chair</button>
+        <button className="mx-4 p-2 bg-[#A5988E] rounded-2xl">
+          Living Room
+        </button>
         <button>Chair</button>
         <button>Arm Chair</button>
       </div>
