18e71db7900fa0c5b03c262e52a79b9a8e241007
AayushMohan <aayushmohan1702@gmail.com>
2023-10-16 23:07:10 +0530

    Updated Button

--- components/Category.tsx
+++ components/Category.tsx
@@ -16,7 +16,7 @@ const Category = (props: Props) => {
         <button className="mx-4 p-2 bg-[#A5988E]/70 text-[#383838] rounded-2xl">
           Arm Chair
         </button>
-        <button className="mx-4 p-2 bg-[#A5988E] text-[#383838] rounded-2xl">
+        <button className="mx-4 p-2 bg-[#A5988E]/70 text-[#383838] rounded-2xl">
           Stool
         </button>
         <button className="mx-4 p-2 bg-[#A5988E] text-[#383838] rounded-2xl">
