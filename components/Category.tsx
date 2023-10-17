9b4c1d57c7409266229484da9922cb8fad1b9d92
AayushMohan <aayushmohan1702@gmail.com>
2023-10-17 23:50:05 +0530

    added alt info

--- components/Category.tsx
+++ components/Category.tsx
@@ -51,7 +51,7 @@ const Category = (props: Props) => {
         />
         <Image
           src="https://images.unsplash.com/photo-1611269154421-4e27233ac5c7?auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHRhYmxlfGVufDB8MXwwfHx8MA%3D%3D&w=800"
-          alt=""
+          alt="table"
           width={360}
           height={360}
         />
