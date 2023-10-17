44d9023ffab572e0094cc6dfa46ddbcf442a16b3
AayushMohan <aayushmohan1702@gmail.com>
2023-10-17 23:51:13 +0530

    Added Border radius

--- components/Category.tsx
+++ components/Category.tsx
@@ -49,6 +49,7 @@ const Category = (props: Props) => {
           alt="table"
           width={300}
           height={300}
+          className="rounded-md"
         />
         <Image
           src="https://images.unsplash.com/photo-1611269154421-4e27233ac5c7?auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHRhYmxlfGVufDB8MXwwfHx8MA%3D%3D&w=800"
