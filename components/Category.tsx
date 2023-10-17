c1f3f80538d65f673daf79395d3cd836204d7a00
AayushMohan <aayushmohan1702@gmail.com>
2023-10-17 23:32:59 +0530

    Added Carousel

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
@@ -25,6 +26,15 @@ const Category = (props: Props) => {
         <button className="mx-4 p-2 bg-[#A5988E]/70 text-[#383838]/90 rounded-2xl">
           Sofa
         </button>
+      </div>
+      <div className="flex items-center justify-center mt-10">
+        <Image
+          src="https://images.unsplash.com/photo-1589535255042-88799bb1b20b?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2787"
+          alt=""
+          width={300}
+          height={300}
+          className=""
+        />
       </div>
     </div>
   );
