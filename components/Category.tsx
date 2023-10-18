5a42e27efa2281a97c2f83b9f1324f2c2bd6a9ef
AayushMohan <aayushmohan1702@gmail.com>
2023-10-18 22:27:57 +0530

    Updated width

--- components/Category.tsx
+++ components/Category.tsx
@@ -31,8 +31,8 @@ const Category = (props: Props) => {
         <Image
           src="https://images.unsplash.com/photo-1589535255042-88799bb1b20b?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2787"
           alt="table"
-          width={300}
-          height={300}
+          width={250}
+          height={250}
           className="mx-4"
         />
 
