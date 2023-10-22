c895508f608c3cf927ea0a944681fd4d0a1f1dbd
AayushMohan <aayushmohan1702@gmail.com>
2023-10-22 23:35:24 +0530

    Added Tastes

--- components/Taste.tsx
+++ components/Taste.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Taste = (props: Props) => {
-  return <div className="h-screen">Choose Your Taste</div>;
+  return (
+    <div className="h-screen mt-80">
+      <h1 className="text-3xl">Choose Your Taste</h1>
+    </div>
+  );
 };
 
 export default Taste;
