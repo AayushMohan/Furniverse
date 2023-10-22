1212c3b1509b48677573da270e1cfc34c4e90b86
AayushMohan <aayushmohan1702@gmail.com>
2023-10-22 23:39:43 +0530

    Added Taste Styling

--- components/Taste.tsx
+++ components/Taste.tsx
@@ -8,7 +8,7 @@ const Taste = (props: Props) => {
     <div className="h-screen mt-80">
       <h1 className="text-3xl text-center font-serif">Choose Your Taste</h1>
 
-      <div>
+      <div className="flex items-center justify-around mt-10 pb-20 bg-inherit overflow-hidden">
         <Image
           src="https://images.unsplash.com/photo-1589535255042-88799bb1b20b?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2787"
           alt="table"
