6f02203dec7eba475743a706ff418f3c1bc29497
AayushMohan <aayushmohan1702@gmail.com>
2023-10-22 23:30:02 +0530

    Updated Styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="bg-[#F0f0F0] h-screen text-[#000]">
         <Header />
         <Hero />
         <Category />
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className="bg-[#F0f0F0] h-screen text-[#000]"></main>;
+  return <main className=""></main>;
 }
