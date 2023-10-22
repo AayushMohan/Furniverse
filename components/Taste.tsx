8025925e5a19d111f07ccfc46fe36ca2ef36ca75
AayushMohan <aayushmohan1702@gmail.com>
2023-10-22 23:41:16 +0530

    Updated Image

--- components/Taste.tsx
+++ components/Taste.tsx
@@ -18,7 +18,7 @@ const Taste = (props: Props) => {
         />
 
         <Image
-          src="https://images.unsplash.com/photo-1581428982868-e410dd047a90?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2787"
+          src="https://images.unsplash.com/photo-1579146510179-6d8a87d24d54?auto=format&fit=crop&q=80&w=2835&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="table"
           width={300}
           height={300}
