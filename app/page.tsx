13d0cba6ad5dfc8faa8f483f5c626107e0f379ed
AayushMohan <aayushmohan1702@gmail.com>
2023-10-22 23:29:30 +0530

    Updated Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import Category from "@/components/Category";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="bg-[#F0f0F0] h-screen text-[#000]">
-      <Header />
-      <Hero />
-      <Category />
-    </main>
-  );
+  return <main className="bg-[#F0f0F0] h-screen text-[#000]"></main>;
 }
