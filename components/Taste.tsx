32fa79771ff125b3cc4eb7c95cc560f50d099af1
AayushMohan <aayushmohan1702@gmail.com>
2023-10-22 23:36:57 +0530

    Added Taste Heading

--- components/Taste.tsx
+++ components/Taste.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Taste = (props: Props) => {
   return (
     <div className="h-screen mt-80">
-      <h1 className="text-3xl">Choose Your Taste</h1>
+      <h1 className="text-3xl text-center font-serif">Choose Your Taste</h1>
     </div>
   );
 };
