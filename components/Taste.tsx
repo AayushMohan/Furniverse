675e2c65331f4537fe4645c295b0da735774f659
AayushMohan <aayushmohan1702@gmail.com>
2023-10-25 23:26:16 +0530

    Added Background

--- components/Taste.tsx
+++ components/Taste.tsx
@@ -62,7 +62,7 @@ const Taste = (props: Props) => {
         <div className="absolute top-44 right-14 flex flex-col font-serif">
           <span className="text-3xl">Discounts Change </span>
           <span className="text-3xl">every three weeks</span>
-          <button className="p-4">More Info</button>
+          <button className="p-4 bg-[#A5988E]/70">More Info</button>
         </div>
       </div>
     </div>
