6bef7e18637074b4ded08ab1f804cce785c37eba
AayushMohan <aayushmohan1702@gmail.com>
2023-11-01 23:13:14 +0530

    Added Share Component

---
+++ components/Share.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Share = (props: Props) => {
+  return (
+    <div>
+      <h1>Share your Home with Us</h1>
+    </div>
+  );
+};
+
+export default Share;
