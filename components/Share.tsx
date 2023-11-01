385d81050ed0ec1fd69ae2673cf97de9e5c036b8
AayushMohan <aayushmohan1702@gmail.com>
2023-11-01 23:18:26 +0530

    Added Image

--- components/Share.tsx
+++ components/Share.tsx
@@ -29,6 +29,13 @@ const Share = (props: Props) => {
           height={250}
           className="mx-4"
         />
+        <Image
+          src="https://plus.unsplash.com/premium_photo-1675744019321-f90d6d719da7?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&  ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2787"
+          alt="table"
+          width={250}
+          height={250}
+          className="mx-4"
+        />
       </div>
     </div>
   );
