948ecde12da0e148b4e95350aa0df79ed3718fc5
AayushMohan <aayushmohan1702@gmail.com>
2023-11-01 23:16:32 +0530

    Added Image

--- components/Share.tsx
+++ components/Share.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
@@ -6,6 +7,15 @@ const Share = (props: Props) => {
   return (
     <div className="h-screen">
       <h1>Share your Home with Us</h1>
+      <div>
+        <Image
+          src="https://plus.unsplash.com/premium_photo-1675744019321-f90d6d719da7?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&  ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2787"
+          alt="table"
+          width={250}
+          height={250}
+          className="mx-4"
+        />
+      </div>
     </div>
   );
 };
