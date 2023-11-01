607a376b544cd8c9f0132800861a323dcf2be197
AayushMohan <aayushmohan1702@gmail.com>
2023-11-01 23:20:23 +0530

    Updated Styling

--- components/Share.tsx
+++ components/Share.tsx
@@ -5,9 +5,9 @@ type Props = {};
 
 const Share = (props: Props) => {
   return (
-    <div className="h-screen">
+    <div className="h-screen mt-40">
       <h1>Share your Home with Us</h1>
-      <div className="flex">
+      <div className="flex justify-between">
         <Image
           src="https://plus.unsplash.com/premium_photo-1675744019321-f90d6d719da7?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&  ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2787"
           alt="table"
