53ebd3e743ee85df2ae821eae5f7105578415db5
AayushMohan <aayushmohan1702@gmail.com>
2023-11-01 23:30:33 +0530

    Updated Image

--- components/Share.tsx
+++ components/Share.tsx
@@ -30,7 +30,7 @@ const Share = (props: Props) => {
           className="mx-4"
         />
         <Image
-          src="https://plus.unsplash.com/premium_photo-1675744019321-f90d6d719da7?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&  ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2787"
+          src="https://images.unsplash.com/photo-1615876063860-d971f6dca5dc?auto=format&fit=crop&q=80&w=2832&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D7"
           alt="table"
           width={250}
           height={250}
