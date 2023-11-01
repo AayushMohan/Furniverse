d753e68e8ed55165cad7715c791572800266984f
AayushMohan <aayushmohan1702@gmail.com>
2023-11-01 23:19:12 +0530

    Added Styling

--- components/Share.tsx
+++ components/Share.tsx
@@ -7,7 +7,7 @@ const Share = (props: Props) => {
   return (
     <div className="h-screen">
       <h1>Share your Home with Us</h1>
-      <div>
+      <div className="flex">
         <Image
           src="https://plus.unsplash.com/premium_photo-1675744019321-f90d6d719da7?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&  ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2787"
           alt="table"
