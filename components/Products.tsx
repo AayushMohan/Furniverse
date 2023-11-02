d7c74b401edb0ef3b9d76b4132d5369c121ba505
AayushMohan <aayushmohan1702@gmail.com>
2023-11-02 23:54:51 +0530

    Add product image to Products component

--- components/Products.tsx
+++ components/Products.tsx
@@ -28,6 +28,13 @@ const Products = (props: Props) => {
           height={450}
           className="mx-2"
         />
+        <Image
+          src="https://images.unsplash.com/photo-1636138389529-d35a2a348dc1?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGluaW5nJTIwdGFibGV8ZW58MHwwfDB8fHww"
+          alt="table"
+          width={450}
+          height={450}
+          className="mx-2"
+        />
       </div>
     </div>
   );
