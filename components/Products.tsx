feecb1515e2854dd2a2b56f1bf896b89a158a1d3
AayushMohan <aayushmohan1702@gmail.com>
2023-11-03 23:04:29 +0530

    Update product image source

--- components/Products.tsx
+++ components/Products.tsx
@@ -15,7 +15,7 @@ const Products = (props: Props) => {
           className="mx-2"
         />
         <Image
-          src="https://images.unsplash.com/photo-1636138389529-d35a2a348dc1?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGluaW5nJTIwdGFibGV8ZW58MHwwfDB8fHww"
+          src="https://images.unsplash.com/photo-1617806118233-18e1de247200?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fGludGVyaW9yJTIwZGVzaWdufGVufDB8MHwwfHx8MA%3D%3D"
           alt="table"
           width={450}
           height={450}
