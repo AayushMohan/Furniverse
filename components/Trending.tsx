a03a07d59e4521b7a4b813c8feded03d34f1f7ee
AayushMohan <aayushmohan1702@gmail.com>
2023-11-05 23:49:16 +0530

    Add image to Trending component

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
@@ -8,7 +9,15 @@ const Trending = (props: Props) => {
       <div>
         <h1>Trending Products</h1>
       </div>
-      <div></div>
+      <div>
+        <Image
+          src="https://images.unsplash.com/photo-1615876063860-d971f6dca5dc?auto=format&fit=crop&q=80&w=2832&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D7"
+          alt="table"
+          width={450}
+          height={450}
+          className="mx-2"
+        />
+      </div>
     </div>
   );
 };
