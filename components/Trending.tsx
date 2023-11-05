709872caf2725bac8b9c8afad345b8cc03e48eaa
AayushMohan <aayushmohan1702@gmail.com>
2023-11-05 23:47:45 +0530

    Add header to Trending component

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -3,7 +3,14 @@ import React from "react";
 type Props = {};
 
 const Trending = (props: Props) => {
-  return <div>Trending</div>;
+  return (
+    <div>
+      <div>
+        <h1>Trending Products</h1>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Trending;
