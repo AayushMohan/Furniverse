f71727f16eebf19961f40b3b08afa00a6b5c30c9
AayushMohan <aayushmohan1702@gmail.com>
2023-11-05 23:53:47 +0530

    Updated Trending component with new image layout

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -11,7 +11,7 @@ const Trending = (props: Props) => {
           Trending Products
         </h1>
       </div>
-      <div className="">
+      <div className="flex justify-between overflow-x-scroll">
         <Image
           src="https://images.unsplash.com/photo-1615876063860-d971f6dca5dc?auto=format&fit=crop&q=80&w=2832&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D7"
           alt="table"
