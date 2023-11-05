fa01ed3578f87bffe1594a64c338210a940662ca
AayushMohan <aayushmohan1702@gmail.com>
2023-11-05 23:51:24 +0530

    Add image of table to Trending component

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -87,6 +87,13 @@ const Trending = (props: Props) => {
           height={450}
           className="mx-2"
         />
+        <Image
+          src="https://images.unsplash.com/photo-1615876063860-d971f6dca5dc?auto=format&fit=crop&q=80&w=2832&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D7"
+          alt="table"
+          width={450}
+          height={450}
+          className="mx-2"
+        />
       </div>
     </div>
   );
