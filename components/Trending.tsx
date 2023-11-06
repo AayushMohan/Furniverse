1cecbd50fa5af0b2e14bc1822e3a4e9b96cae32c
AayushMohan <aayushmohan1702@gmail.com>
2023-11-06 23:41:18 +0530

    Update Trending component image source

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -27,7 +27,7 @@ const Trending = (props: Props) => {
           className="mx-2"
         />
         <Image
-          src="https://images.unsplash.com/photo-1615876063860-d971f6dca5dc?auto=format&fit=crop&q=80&w=2832&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D7"
+          src="https://images.unsplash.com/photo-1604578762246-41134e37f9cc?q=80&w=2908&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="table"
           width={450}
           height={450}
