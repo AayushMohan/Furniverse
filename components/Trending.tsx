87751dcc416a4f91f7252a0d7d893e4e7008ddbb
AayushMohan <aayushmohan1702@gmail.com>
2023-11-06 23:47:59 +0530

    Remove unnecessary image in Trending component

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -47,13 +47,6 @@ const Trending = (props: Props) => {
           height={450}
           className="mx-2"
         />
-        <Image
-          src="https://images.unsplash.com/photo-1594809512566-021e8369702a?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="table"
-          width={450}
-          height={450}
-          className="mx-2"
-        />
 
         <Image
           src="https://images.unsplash.com/photo-1589135003135-2a30f21cd08c?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
