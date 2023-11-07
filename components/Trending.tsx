12d6d8f6912167ba5a401797bb66178bc5dd5152
AayushMohan <aayushmohan1702@gmail.com>
2023-11-07 23:52:22 +0530

    Update Trending component image source

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -13,7 +13,7 @@ const Trending = (props: Props) => {
       </div>
       <div className="flex justify-between overflow-x-scroll">
         <Image
-          src="https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.unsplash.com/photo-1602872030490-4a484a7b3ba6?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njh8fGZ1cm5pdHVyZXN8ZW58MHwwfDB8fHww"
           alt="table"
           width={300}
           height={250}
