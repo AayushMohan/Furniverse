299c4d7ed067180bc4bcd0131fe8e7988dd0d164
AayushMohan <aayushmohan1702@gmail.com>
2023-11-07 23:47:18 +0530

    Update image width in Trending component

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -15,7 +15,7 @@ const Trending = (props: Props) => {
         <Image
           src="https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="table"
-          width={250}
+          width={300}
           height={250}
           className="mx-2"
         />
