85cacbe142c9754b97c7757343427794b3e463b7
AayushMohan <aayushmohan1702@gmail.com>
2023-11-28 23:52:29 +0530

    Remove unnecessary code in Trending component

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -89,19 +89,6 @@ const Trending = (props: Props) => {
           <span>Sofa</span>
           <span>$1900</span>
         </div>
-
-        <div>
-          <Image
-            src="https://images.unsplash.com/photo-1549187774-b4e9b0445b41?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="table"
-            width={400}
-            height={350}
-            className="mx-2"
-          />
-          <span>Best Seller</span>
-          <span>Sofa</span>
-          <span>$3000</span>
-        </div>
       </div>
     </div>
   );
