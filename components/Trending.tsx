b3d211ec99e8c6fd99b16c75dce88c3b473da55b
AayushMohan <aayushmohan1702@gmail.com>
2023-11-30 23:05:46 +0530

    Update image margin in Trending component

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -71,7 +71,7 @@ const Trending = (props: Props) => {
               alt="table"
               width={400}
               height={350}
-              className="mx-2"
+              className="mx-20"
             />
             <span>Best Seller</span>
             <span>Sofa</span>
